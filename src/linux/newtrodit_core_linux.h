53eaad1290e8db64c41fbc26d3f02a73c6bb1fe3
ZackeryRSmith <zackery.smith82307@gmail.com>
2022-06-24 15:20:58 -0400

    :bug: Refix what once was fixed...

--- src/linux/newtrodit_core_linux.h
+++ src/linux/newtrodit_core_linux.h
@@ -297,30 +297,28 @@ void CanonOff()
 }
 
 /* Convert hex to ANSI (BIOS to ANSI standards)*/
-char *HexToAnsi(int num)
+char* HexToAnsi(int num)
 {
-  char hex_table[16] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
-                        0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
-  char ansi_table[16] = {30, 34, 32, 36, 31, 35, 33, 37,
-                         90, 94, 91, 95, 92, 96, 93, 97};
-  char *buf = (char *)calloc(1, sizeof(char) * 32); // Allocate enough memory
-  if (num < 0 || num > 0xff)                        // Keep a valid number range
-  {
-    return NULL;
-  }
-  int font = 0, background = 0;
-  if (num > 0xf)
-  {
-    font = ansi_table[num >> 4];
-    background = ansi_table[num & 0xf] + 10;
-    snprintf(buf, 32 * sizeof(char), "\x1b[%d;%dm", background, font);
-  }
-  else
-  {
-    font = ansi_table[num];
-    snprintf(buf, 32 * sizeof(char), "\x1b[%dm", font);
-  }
-  return buf;
+    char hex_table[16] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};
+    char ansi_table[16] = {30, 34, 32, 36, 31, 35, 33, 37, 90, 94, 91, 95, 92, 96, 93, 97};
+    char* buf = (char*)calloc(1, sizeof(char) * 32); // Allocate enough memory
+    if (num < 0 || num > 0xff) // Keep a valid number range
+    {
+        return NULL;
+    }
+    int font = 0, background = 0;
+    if (num > 0xf)
+    {
+        font = ansi_table[num >> 4] + 10;
+        background = ansi_table[num & 0xf];
+        snprintf(buf, 32*sizeof(char), "\x1b[%d;%dm", font, background);
+    }
+    else
+    {
+        font = ansi_table[num];
+        snprintf(buf, 32*sizeof(char), "\x1b[%dm", font);
+    }
+    return buf;
 }
 
 /* reads from keypress, returns keycode */
